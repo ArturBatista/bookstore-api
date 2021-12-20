b4c0aca0fb51828b91209132df6fd5bbb8464558
ArturBatista <artur.batista07@gmail.com>
2021-12-20 19:46:35 -0300

    instancia de forma automatica da base de dados

--- src/main/java/com/artur/bookstore/domain/Livro.java
+++ src/main/java/com/artur/bookstore/domain/Livro.java
@@ -3,12 +3,14 @@ package com.artur.bookstore.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+@Entity
 public class Livro implements Serializable{
 
 	/**
