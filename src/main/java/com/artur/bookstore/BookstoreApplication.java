9e3e63cfa65521a9274817e1100da195487fe533
ArturBatista <artur.batista07@gmail.com>
2021-12-20 12:49:02 -0300

    Projeto Criado

---
+++ src/main/java/com/artur/bookstore/BookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.artur.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+}
