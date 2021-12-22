9f006dcf301cb86851c1011cdac260dfae5b4e1e
ArturBatista <artur.batista07@gmail.com>
2021-12-22 19:35:29 -0300

    adicionado anotation

--- src/main/java/com/artur/bookstore/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/artur/bookstore/resources/exceptions/ResourceExceptionHandler.java
@@ -4,10 +4,12 @@ import javax.servlet.ServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.artur.bookstore.service.exceptions.ObjectNotFoundException;
 
+@ControllerAdvice
 public class ResourceExceptionHandler {
 	
 	@ExceptionHandler(ObjectNotFoundException.class)
