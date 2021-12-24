b2987f953c352046e7ff37e8f297d3a60a733c48
ArturBatista <artur.batista07@gmail.com>
2021-12-24 08:32:27 -0300

    Listando todos os Livros por Categoria

--- src/main/java/com/artur/bookstore/resources/LivroResource.java
+++ src/main/java/com/artur/bookstore/resources/LivroResource.java
@@ -1,14 +1,19 @@
 package com.artur.bookstore.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.artur.bookstore.domain.Categoria;
 import com.artur.bookstore.domain.Livro;
+import com.artur.bookstore.dtos.LivroDTO;
 import com.artur.bookstore.service.CategoriaService;
 import com.artur.bookstore.service.LivroService;
 
@@ -23,6 +28,13 @@ public class LivroResource {
 	public ResponseEntity<Livro> findById(@PathVariable Integer id){
 		Livro livro = service.findById(id);
 		return ResponseEntity.ok().body(livro);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<LivroDTO>> findAll(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat){
+		List<Livro> list = service.findAll(id_cat);
+		List<LivroDTO> listDTO = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
--- src/main/java/com/artur/bookstore/service/LivroService.java
+++ src/main/java/com/artur/bookstore/service/LivroService.java
@@ -1,11 +1,13 @@
 package com.artur.bookstore.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.artur.bookstore.domain.Livro;
+import com.artur.bookstore.repositories.CategoriaRepository;
 import com.artur.bookstore.repositories.LivroRepository;
 import com.artur.bookstore.service.exceptions.ObjectNotFoundException;
 
@@ -15,9 +17,17 @@ public class LivroService {
 	@Autowired
 	LivroRepository livroRepository;
 	
+	@Autowired
+	CategoriaService categoriaService;
+	
 	public Livro findById(int id) {
 		Optional<Livro> livro = livroRepository.findById(id);
 		return livro.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado Id: " + id + ", Tipo: " + Livro.class.getName()));
+	}
+
+	public List<Livro> findAll(Integer id_cat) {
+		categoriaService.findById(id_cat);
+		return livroRepository.findAllByCategoria(id_cat);
 	}
 
 }
