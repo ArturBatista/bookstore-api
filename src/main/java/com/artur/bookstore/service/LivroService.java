2a5c3d834bc0169028aea0914613820b24a4e72c
ArturBatista <artur.batista07@gmail.com>
2021-12-24 08:54:32 -0300

    PUT e PATH para Livro

--- src/main/java/com/artur/bookstore/service/LivroService.java
+++ src/main/java/com/artur/bookstore/service/LivroService.java
@@ -30,4 +30,18 @@ public class LivroService {
 		return livroRepository.findAllByCategoria(id_cat);
 	}
 
+	public Livro update(Integer id, Livro obj) {
+		Livro newObj = findById(id);
+		updateData(newObj, obj);
+		
+		return livroRepository.save(newObj);
+	}
+
+	private void updateData(Livro newObj, Livro obj) {
+		newObj.setTitulo(obj.getTitulo());
+		newObj.setNome_autor(obj.getNome_autor());
+		newObj.setTitulo(obj.getTitulo());
+		
+	}
+
 }
