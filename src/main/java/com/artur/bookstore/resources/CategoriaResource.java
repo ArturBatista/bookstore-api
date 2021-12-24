426b1ad5f461858536394e933914c44f330285dc
ArturBatista <artur.batista07@gmail.com>
2021-12-24 07:35:07 -0300

    UPDATE para Categoria

--- src/main/java/com/artur/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/artur/bookstore/resources/CategoriaResource.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,5 +48,10 @@ public class CategoriaResource {
 		//return ResponseEntity.created(uri).body(obj);
 		return ResponseEntity.created(uri).build();
 	}
- 
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CategoriaDTO> update(@PathVariable Integer id, @RequestBody CategoriaDTO objDTO){
+		Categoria newObj = service.update(id, objDTO);
+		return ResponseEntity.ok().body(new CategoriaDTO(newObj));
+	} 
 }
--- src/main/java/com/artur/bookstore/service/CategoriaService.java
+++ src/main/java/com/artur/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.artur.bookstore.domain.Categoria;
+import com.artur.bookstore.dtos.CategoriaDTO;
 import com.artur.bookstore.repositories.CategoriaRepository;
 import com.artur.bookstore.service.exceptions.ObjectNotFoundException;
 
@@ -28,6 +29,13 @@ public class CategoriaService {
 	
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDTO) {
+		Categoria obj = findById(id);
+		obj.setNome(objDTO.getNome());
+		obj.setDescricao(objDTO.getDescricao());
 		return repository.save(obj);
 	}
 }
